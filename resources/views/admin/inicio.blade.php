2d484c5c8587361eee4a8158f2c1d4a6cfa71127
Slender5 <44220732+Slender5@users.noreply.github.com>
2020-03-07 15:49:42 -0500

    Añadiendo controller e Inicio

---
+++ resources/views/admin/inicio.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<div>
+		<ul>
+			<li>Inicio</li>
+			<li>Conocenos</li>
+			<li>Infraestructura</li>
+			<li>Academicos</li>
+			<li>Galeria</li>
+		</ul>
+	</div>
+	<div>
+		<button>Editar</button>
+		<br>
+		<br>
+		<img src="https://www.anmosugoi.com/wp-content/uploads/2019/08/anime-de-Dr.-Stone-destacada-el-palomitron.jpg">
+	</div>
+	<div>
+		<h1>Noticia</h1>
+		<img src="">
+		<p></p>
+		<button>Editar</button>
+		<button>Eliminar</button>
+	</div>
+</body>
+</html>
