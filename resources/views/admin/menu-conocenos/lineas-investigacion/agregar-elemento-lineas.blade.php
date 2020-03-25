cac766fcee623b4768a060b36ad2b8422323d3e9
WesterlyJoker <55644863+WesterlyJoker@users.noreply.github.com>
2020-03-25 15:40:52 -0500

    Update agregar-elemento-lineas.blade.php

--- resources/views/admin/menu-conocenos/lineas-investigacion/agregar-elemento-lineas.blade.php
+++ resources/views/admin/menu-conocenos/lineas-investigacion/agregar-elemento-lineas.blade.php
@@ -29,31 +29,6 @@
           <input type="text" name="" id ="informacion-perfil" value="" placeholder="Clave Línea de Investigación TECNM" class="form-control">
     </div>
 
-    <div class="formulario"> 
-          <label for="">Nombre del CA</label>
-          <input type="text" name="" id ="informacion-perfil" value="" placeholder="Nombre del CA" class="form-control">
-    </div>
-
-    <div class="formulario"> 
-          <label for="">Grado del CA</label>
-          <input type="text" name="" id ="informacion-perfil" value="" placeholder="Grado del CA" class="form-control">
-    </div>
-
-    <div class="formulario"> 
-          <label for="">ID CA</label>
-          <input type="text" name="" id ="informacion-perfil" value="" placeholder="ID CA" class="form-control">
-    </div>
-
-    <div class="formulario"> 
-          <label for="">Clave CA</label>
-          <input type="text" name="" id ="informacion-perfil" value="" placeholder="Clave CA" class="form-control">
-    </div>
-
-    <div class="formulario"> 
-          <label for="">Integrantes</label>
-          <input type="text" name="" id ="informacion-perfil" value="" placeholder="Nombre del Integrante" class="form-control">
-    </div>
-
     <div class="contenedor-botones">
                 
         <button class="btn"><span>Guardar</span></button>
