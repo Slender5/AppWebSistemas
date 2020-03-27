577d5b0d9bf14c92a29424f9420338579a3e59b4
AMoraHE <55561577+AMoraHE@users.noreply.github.com>
2020-03-26 20:36:35 -0500

    Delete

--- public/js/btn-cargar.js
+++
@@ -1,6 +0,0 @@
-/****************************************CARGAR ARCHIVOS*******************************************/
-
-function cambiar(){
-    var pdrs = document.getElementById('file-upload').files[0].name;
-    document.getElementById('info-img').innerHTML = pdrs;
-}
