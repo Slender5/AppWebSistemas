b318b136077c5081e236990e4ce1ac022c62f85d
WesterlyJoker <55644863+WesterlyJoker@users.noreply.github.com>
2020-03-26 19:09:28 -0500

    Update crear-noticia.blade.php

--- resources/views/admin/menu-inicio/noticias/crear-noticia.blade.php
+++ resources/views/admin/menu-inicio/noticias/crear-noticia.blade.php
@@ -14,12 +14,15 @@
 				<h3>Crear Noticias</h3>
 
 		</div>
+        
+     <form class="form-goup"  files = "true" method="POST" action="/noticias-admin/" enctype="multipart/form-data">
+             @csrf
 
 
 		<div class="formulario">
 			
 			<label for="">Titulo</label>
-			<input type="text" name="" id="titulo" value="" placeholder="Inserte Titulo Deseado" class="form-control">
+			<input type="text" name="titulo" id="titulo" value="" placeholder="Inserte Titulo Deseado" class="form-control">
 
 		</div>
 
@@ -37,7 +40,7 @@
 						<i class="fas fa-cloud-upload-alt"></i> Subir Archivo
 					</label>
 						
-					<input id="file-upload" onchange='cambiar()' type="file" style='display: none;'/>
+					<input id="file-upload" onchange='cambiar()' name="image" type="file" style='display: none;'/>
 
 					<div id="info-img">No se Eligió Archivo</div>
 					
@@ -47,7 +50,7 @@
 
 				<div class="contenedor-img-seccion">
 					
-					<img src="Img/sistemas.png">
+					<img src="img/sistemas.png">
 
 				</div>
 					
@@ -57,16 +60,18 @@
 		<div class="formulario">
 			
 			<label for="">Descripción Noticia</label>
-			<textarea placeholder="Inserte Descripción Deseada ... "></textarea>
+			<textarea name="redaccion" placeholder="Inserte Descripción Deseada ... "></textarea>
 			
 		</div>
 
 		<div class="contenedor-botones">
 					
-			<button class="btn"><span>Guardar</span></button>
-			<button class="btn"><span>Cancelar</span></button>
+			<button class="btn" type="submit"><span>Guardar</span></button>
+			<a href="/noticias-admin/"><button class="btn"><span>Cancelar</span></button></a>
 
 		</div>
+        
+        </form>
 
 	</div>
 
